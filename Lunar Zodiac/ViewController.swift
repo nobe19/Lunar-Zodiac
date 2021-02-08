7f062fe065b78e5219a342f9b4e4aee267674d91
gallaugher <john.gallaugher@bc.edu>
2021-02-08 18:14:04 -0500

    Initial Commit

---
+++ Lunar Zodiac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lunar Zodiac
+//
+//  Created by John Gallaugher on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
